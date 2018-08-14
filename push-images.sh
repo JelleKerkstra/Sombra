2745160a5a578c812886e7c891256203e5bf1e15
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-08-14 14:45:25 +1200

    Push images to docker hub

---
+++ push-images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+images=$(docker image ls --format "{{.Repository}}" | grep -P "^($DC_REPOSITORY/sombra\.).*(?<!unittests)$")
+for image in $images; do
+	docker push $image
+done
+$SHELL
