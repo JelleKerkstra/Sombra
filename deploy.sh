1abfb645d1d500f5f306ae59e85dd4eb1aea9fdc
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-08-16 16:11:46 +1200

    Make regex case insensitive

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-images=$(docker image ls --format "{{.Repository}}" | grep -P "^($DC_REPOSITORY/sombra\.).*(?<!unittests)$")
+images=$(docker image ls --format "{{.Repository}}" | grep -P -i "^($DC_REPOSITORY/sombra\.).*(?<!unittests)$")
 for image in $images; do
 	docker push $image
 done
