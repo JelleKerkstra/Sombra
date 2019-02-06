bbb89d9f1f86147e4352fb3b3f0f774b9bd64922
Jelle Kerkstra <jellekerkstra@hotmail.com>
2019-02-06 10:05:42 +0100

    Fix bug

--- Sombra.Messaging/Infrastructure/IAsyncRequestHandler.cs
+++ Sombra.Messaging/Infrastructure/IAsyncRequestHandler.cs
@@ -2,7 +2,7 @@
 
 namespace Sombra.Messaging.Infrastructure
 {
-    internal interface IAsyncRequestHandler<in TRequest, TResponse>
+    public interface IAsyncRequestHandler<in TRequest, TResponse>
         where TRequest : IRequest<TResponse>
         where TResponse : class, IResponse, new()
     {
