694d56e0be7ff1396a6518e7d4af46bbfbf30607
Jelle Kerkstra <jellekerkstra@hotmail.com>
2019-02-06 12:19:07 +0100

    Add code for debugging purposes

--- Sombra.Messaging/Infrastructure/CustomAutoSubscriber.cs
+++ Sombra.Messaging/Infrastructure/CustomAutoSubscriber.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using EasyNetQ;
 using EasyNetQ.AutoSubscribe;
@@ -15,9 +18,23 @@ namespace Sombra.Messaging.Infrastructure
         public override void SubscribeAsync(params Type[] consumerTypes)
         {
             var genericBusSubscribeMethod = GetSubscribeMethodOfBus(nameof(IBus.SubscribeAsync), typeof(Func<,>));
-            var subscriptionInfos = GetSubscriptionInfos(consumerTypes, typeof(IAsyncEventHandler<>));
+            var subscriptionInfos = GetSubscriptionInfos(consumerTypes, typeof(IAsyncEventHandler<>)).ToList();
             Type SubscriberTypeFromMessageTypeDelegate(Type messageType) => typeof(Func<,>).MakeGenericType(messageType, typeof(Task));
             InvokeMethods(subscriptionInfos, DispatchAsyncMethodName, genericBusSubscribeMethod, SubscriberTypeFromMessageTypeDelegate);
+        }
+
+        protected override IEnumerable<KeyValuePair<Type, AutoSubscriberConsumerInfo[]>> GetSubscriptionInfos(IEnumerable<Type> types, Type interfaceType)
+        {
+            foreach (var concreteType in types.Where(t => t.GetTypeInfo().IsClass && !t.GetTypeInfo().IsAbstract))
+            {
+                var subscriptionInfos = concreteType.GetInterfaces()
+                    .Where(i => i.GetTypeInfo().IsGenericType && i.GetGenericTypeDefinition() == interfaceType && !i.GetGenericArguments()[0].IsGenericParameter)
+                    .Select(i => new AutoSubscriberConsumerInfo(concreteType, i, i.GetGenericArguments()[0]))
+                    .ToArray();
+
+                if (subscriptionInfos.Any())
+                    yield return new KeyValuePair<Type, AutoSubscriberConsumerInfo[]>(concreteType, subscriptionInfos);
+            }
         }
     }
 }
